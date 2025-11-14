a391232be2f733ed3e95396982945e734ea516b2
edgartulcan <etulcnlvarez@byupathway.edu>
2025-11-14 13:04:21 -0500

    place

---
+++ scripts/place.js
@@ -0,0 +1,5 @@
+// Fecha actual en el copyright
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Fecha de última modificación del documento
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;

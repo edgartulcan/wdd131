27c69141efcd9dd06715ad2ad9a00e05a9558788
edgartulcan <etulcnlvarez@byupathway.edu>
2025-11-14 13:44:59 -0500

    new

--- scripts/place.js
+++ scripts/place.js
@@ -1,5 +1,24 @@
-// Fecha actual en el copyright
-document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
 
-// Fecha de última modificación del documento
-document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
+// WEATHER VALUES (estáticos por ahora)
+const temperature = parseFloat(document.getElementById("temp").textContent);
+const windSpeed = parseFloat(document.getElementById("speed").textContent);
+
+// Wind Chill Formula (Celsius)
+function calculateWindChill(t, v) {
+  return (
+    13.12 +
+    0.6215 * t -
+    11.37 * Math.pow(v, 0.16) +
+    0.3965 * t * Math.pow(v, 0.16)
+  );
+}
+
+// VALIDATION RULES (Metric)
+if (temperature <= 10 && windSpeed > 4.8) {
+  const wc = calculateWindChill(temperature, windSpeed).toFixed(1);
+  document.getElementById("windchill").textContent = `${wc} °C`;
+} else {
+  document.getElementById("windchill").textContent = "N/A";
+}
